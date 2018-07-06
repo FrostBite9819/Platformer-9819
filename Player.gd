0131f7cb4a5953c6b6b1669fdb83e652b27cd291
User VGP-03 <VGP-03>
2018-07-06 11:16:32 -0400

    final

--- Player.gd
+++ Player.gd
@@ -6,6 +6,9 @@ const JUMPHEIGHT = -550
 
 var motion = Vector2()
 
+func _ready():
+	add_to_group("Player")
+
 func _physics_process(delta):
 	motion.y += GRAVITY
 	
@@ -39,7 +42,7 @@ func _physics_process(delta):
 func _on_PlayerBody_body_entered(body):
 	if body.is_in_group("enemies"):
 		get_tree().change_scene("res://World.tscn")
+	elif body.is_in_group("portal"):
+		get_tree().change_scene("res://Win Screen.tscn")
 	else:
 		pass
-	
-	
--- StaticBody2D.gd
+++ StaticBody2D.gd
@@ -1,8 +1,1 @@
 extends StaticBody2D
-
-func _ready():
-	hide()
-	add_to_group("borders2")
-	pass
-func _physics_process(delta):
-	print("hey")
